310c260480402342816f21e42aa0ecce5f66d37b
Pierre Molinaro <pierre@pcmolinaro.name>
2020-11-13 12:09:24 +0100

    Amélioration curseurs

--- other-projects/build-png-images/build-png-images/RotationCursor.swift
+++ other-projects/build-png-images/build-png-images/RotationCursor.swift
@@ -11,6 +11,31 @@ import Cocoa
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
+private func drawRotationArrow (width inWidth : CGFloat) -> NSBezierPath {
+  let bp = NSBezierPath ()
+  bp.lineWidth = inWidth
+  bp.lineCapStyle = .round
+//--- Curve
+  bp.move (to: NSPoint (x: 2.0, y: 8.0))
+  bp.curve (
+    to: NSPoint (x: 14.0, y: 8.0),
+    controlPoint1: NSPoint (x:  2.0, y: 16.0),
+    controlPoint2: NSPoint (x: 14.0, y: 16.0)
+)
+//--- Arrows
+  bp.move (to: NSPoint (x: 2.0, y: 8.0))
+  bp.line (to: NSPoint (x: 0.0, y: 10.0))
+  bp.move (to: NSPoint (x: 2.0, y: 8.0))
+  bp.line (to: NSPoint (x: 4.0, y: 10.0))
+  bp.move (to: NSPoint (x: 14.0, y: 8.0))
+  bp.line (to: NSPoint (x: 12.0, y: 10.0))
+  bp.move (to: NSPoint (x: 14.0, y: 8.0))
+  bp.line (to: NSPoint (x: 16.0, y: 10.0))
+  return bp
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
 class RotationCursor : NSView {
 
   //····················································································································
@@ -24,45 +49,16 @@ class RotationCursor : NSView {
   override func draw (_ inDirtyRect : NSRect) {
 //    NSColor.yellow.setFill ()
 //    __NSRectFill (inDirtyRect)
+    drawArrow ()
   //---
+    let af = AffineTransform (translationByX: 13.0, byY: 7.0)
+    var bp = drawRotationArrow (width: 3.0)
+    bp.transform (using: af)
+    NSColor.white.setStroke ()
+    bp.stroke ()
+    bp = drawRotationArrow (width: 1.0)
+    bp.transform (using: af)
     NSColor.black.setStroke ()
-    let bp = NSBezierPath ()
-    bp.lineWidth = 1.0
-    bp.lineCapStyle = .round
-  //--- Horizontal lines
-    bp.move (to: NSPoint (x: 2.0, y: 8.0))
-    bp.curve (
-      to: NSPoint (x: 14.0, y: 8.0),
-      controlPoint1: NSPoint (x:  2.0, y: 16.0),
-      controlPoint2: NSPoint (x: 14.0, y: 16.0)
-  )
-  //--- Arrows
-    bp.move (to: NSPoint (x: 2.0, y: 8.0))
-    bp.line (to: NSPoint (x: 0.0, y: 10.0))
-    bp.move (to: NSPoint (x: 2.0, y: 8.0))
-    bp.line (to: NSPoint (x: 4.0, y: 10.0))
-    bp.move (to: NSPoint (x: 14.0, y: 8.0))
-    bp.line (to: NSPoint (x: 12.0, y: 10.0))
-    bp.move (to: NSPoint (x: 14.0, y: 8.0))
-    bp.line (to: NSPoint (x: 16.0, y: 10.0))
-//  //--- Vertical lines
-//    bp.move (to: NSPoint (x: 8.0, y: 0.0))
-//    bp.line (to: NSPoint (x: 8.0, y: 16.0))
-//    bp.move (to: NSPoint (x: 7.0, y: 1.0))
-//    bp.line (to: NSPoint (x: 7.0, y: 15.0))
-//    bp.move (to: NSPoint (x: 9.0, y: 1.0))
-//    bp.line (to: NSPoint (x: 9.0, y: 15.0))
-//  //--- Vertical arrows
-//    bp.move (to: NSPoint (x: 8.0, y: 0.0))
-//    bp.line (to: NSPoint (x: 5.0, y: 3.0))
-//    bp.move (to: NSPoint (x: 8.0, y: 0.0))
-//    bp.line (to: NSPoint (x: 11.0, y: 3.0))
-//    bp.move (to: NSPoint (x: 8.0, y: 16.0))
-//    bp.line (to: NSPoint (x: 5.0, y: 13.0))
-//    bp.move (to: NSPoint (x: 8.0, y: 16.0))
-//    bp.line (to: NSPoint (x: 11.0, y: 13.0))
-
-  //---
     bp.stroke ()
   }
 
